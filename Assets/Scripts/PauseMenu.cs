3f5f95117c1641cb51f1a922f32a1ac25852384c
Alexandru Coman <coman.alexandru88@gmail.com>
2022-10-11 00:57:46 +0300

    Push to remote repo

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+	}
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Debug.Log("Pausing game");
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Returning to main menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float speed = 5f;
+	
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        transform.Rotate(speed, 0, 0);
+	}
+}
